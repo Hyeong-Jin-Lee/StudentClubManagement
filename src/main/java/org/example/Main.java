bc34c98bdb30b32adfcb3886d5e16a63fb1a9f87
Hyeongjin lee <gudwls2777@naver.com>
2024-11-28 03:55:50 +0900

    add connect mysql

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+    private static String IP = "192.168.56.101";
+    private static String PORT = "4567";
+    private static String DB_NAME = "sw_club";
+    private static final String URL = "jdbc:mysql://"+IP+":"+PORT+"/"+DB_NAME+"?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to the database!");
+        } catch (SQLException e) {
+            System.out.println("Database connection failed: " + e.getMessage());
+        }
     }
 }
