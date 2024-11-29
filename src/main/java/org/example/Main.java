3952ea213b057599786d27c94ed03bffc0575a7d
Hyeongjin lee <gudwls2777@naver.com>
2024-11-30 01:33:00 +0900

    add quit funtion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,9 @@ package org.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     private static String IP = "192.168.56.101";
@@ -12,12 +14,66 @@ public class Main {
     private static final String USER = "";
     private static final String PASSWORD = "";
 
+    private static Connection conn = null;
     public static void main(String[] args) {
         try {
             Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Connected to the database!");
+
+
+
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                printMenu();
+                System.out.print("Enter your choice: ");
+                int choice = scanner.nextInt();
+                scanner.nextLine(); // Consume newline character
+
+                switch (choice) {
+                    case 1:
+                        connectToDatabase();
+                        break;
+                    case 99:
+                        System.out.println("Exiting system...");
+                        closeConnection();
+                        System.exit(0);
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                }
+            }
         } catch (SQLException e) {
             System.out.println("Database connection failed: " + e.getMessage());
+        }
+    }
+    private static void printMenu() {
+        System.out.println("----------------------------");
+        System.out.println("1. connection");
+        System.out.println("99. quit");
+        System.out.println("----------------------------");
+    }
+
+    private static void connectToDatabase() {
+        try {
+            if (conn == null || conn.isClosed()) {
+                conn = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Database connected successfully!");
+            } else {
+                System.out.println("Already connected to the database.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to the database.");
+            e.printStackTrace();
+        }
+    }
+
+    private static void closeConnection() {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+                System.out.println("Database connection closed.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to close the connection.");
+            e.printStackTrace();
         }
     }
 }
